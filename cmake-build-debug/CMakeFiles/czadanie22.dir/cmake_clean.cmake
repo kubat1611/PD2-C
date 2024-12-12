5f6339d09188ee082e410b675d7474fbb6832834
Mund3k <kubat1611@gmail.com>
2024-12-12 10:22:34 +0100

    /

---
+++ cmake-build-debug/CMakeFiles/czadanie22.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/czadanie22.dir/main.c.obj"
+  "CMakeFiles/czadanie22.dir/main.c.obj.d"
+  "czadanie22.exe"
+  "czadanie22.exe.manifest"
+  "czadanie22.pdb"
+  "libczadanie22.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/czadanie22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
